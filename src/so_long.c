9e7f2541ad5705c34aa737366e6165f5d86540ef
DriQii <enzovella6603@gmail.com>
2023-11-21 23:35:59 +0100

    test

--- src/so_long.c
+++ src/so_long.c
@@ -5,70 +5,22 @@
 
 typedef struct	s_data {
 	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
+	char	*relative_path;
+    int		img_width;
+    int		img_height;
 }				t_data;
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
 
 int main(void)
 {
     void    *mlx;
     void    *mlx_win;
     t_data  img;
-    int     i;
-    int     x;
-    int     y;
-    int j;
 
-    j = 0;
-    x = 800;
-    y = 800;
-    i = 0;
+    img.relative_path = "images/test.xpm";
     mlx = mlx_init();
     mlx_win = mlx_new_window(mlx, 1920, 1080, "So long");
-    img.img = mlx_new_image(mlx, 1980, 1080);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    //my_mlx_pixel_put(&img, 5, 5, 0x00FFFF00);
-
-    while (j < 13)   
-    {   
-        my_mlx_pixel_put(&img, x, y, 0x00F1C284);
-        x++;
-        j++;
-    }
-    x -= j;
-    j = 0;
-    y++;
-    while (j < 5)   
-    {   
-        my_mlx_pixel_put(&img, x, y, 0x00C97E4F);
-        x++;
-        j++;
-    }
-    my_mlx_pixel_put(&img, x, y, 0x00F1C284);
-    x++;
-    j++;
-    while (j < 12)   
-    {   
-        my_mlx_pixel_put(&img, x, y, 0x0F1F0EE);
-        x++;
-        j++;
-    }
-
-    x -= j;
-    j = 0;
-    y++;
-
-    
-    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    img.img = mlx_xpm_file_to_image(mlx, img.relative_path, &img.img_width, &img.img_height);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 500, 500);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 800, 800);
 	mlx_loop(mlx);
 }
