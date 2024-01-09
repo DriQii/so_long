1c7133cafab51b04b6cb8049be944f48591c0bc3
Enzo Vella <DriQii>
2024-01-09 19:56:52 +0100

    norm

--- src/road_error.c
+++ src/road_error.c
@@ -6,7 +6,7 @@
 /*   By: evella <evella@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 14:41:57 by evella            #+#    #+#             */
-/*   Updated: 2024/01/09 11:26:46 by evella           ###   ########.fr       */
+/*   Updated: 2024/01/09 19:48:21 by evella           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,60 +113,74 @@ t_coords	*ft_zero_check(t_character character, char **map, char **newmap)
 				+ i.y, NULL));
 	return (NULL);
 }
-
-int	ft_road_error(char **map, char **newmap, t_character *character)
+static void	ft_check_road(char **map, char **newmap, t_character *character)
 {
 	t_coords	*find;
 	t_coords	*tmp;
-	t_index		i;
+	find = ft_zero_check(*character, map, newmap);
+	if (find)
+	{
+		character->P_coords = ft_newlstcoords(find->x, find->y,
+				character->P_coords);
+		newmap[find->y - 1][find->x - 1] = '1';
+		free(find->next);
+		free(find);
+	}
+	else
+	{
+		tmp = character->P_coords;
+		character->P_coords = character->P_coords->next;
+		free(tmp);
+	}
+}
+static int	ft_check_exit(int c_count, char **newmap, t_character *character)
+{
+	t_coords	*find;
 
-	i.i = 0;
-	i.j = 0;
+	if (c_count == character->C + 1)
+	{
+		find = ft_collectible_check(character->P_coords,
+				character->E_coords, newmap);
+		if (find)
+			return (1);
+		newmap[character->P_coords->y - 1][character->P_coords->x
+			- 1] = '1';
+	}
+	return (0);
+}
+
+static int ft_check_c(char **map, char **newmap, t_character *character, int c_count)
+{
+	t_coords	*find;
+	find = ft_collectible_check(character->P_coords, \
+	character->C_coords, newmap);
+	if (find)
+	{
+		character->P_coords = ft_newlstcoords(find->x, find->y,
+				character->P_coords);
+		newmap[find->y - 1][find->x - 1] = '1';
+		if (c_count < character->C)
+			c_count++;
+	}
+	else
+		ft_check_road(map, newmap, character);
+	return (c_count);
+}
+int	ft_road_error(char **map, char **newmap, t_character *character)
+{
+
+	int			c_count;
+
+	c_count = 0;
 	while (character->P_coords)
 	{
-		if (i.i == character->C + 1)
-		{
-			find = ft_collectible_check(character->P_coords,
-					character->E_coords, newmap);
-			if (find)
-			{
-				return (0);
-			}
-			newmap[character->P_coords->y - 1][character->P_coords->x
-				- 1] = '1';
-		}
-		find = ft_collectible_check(character->P_coords, \
-		character->C_coords, newmap);
-		if (find)
-		{
-			character->P_coords = ft_newlstcoords(find->x, find->y,
-					character->P_coords);
-			newmap[find->y - 1][find->x - 1] = '1';
-			if (i.i < character->C)
-				i.i++;
-		}
-		else
-		{
-			find = ft_zero_check(*character, map, newmap);
-			if (find)
-			{
-				character->P_coords = ft_newlstcoords(find->x, find->y,
-						character->P_coords);
-				newmap[find->y - 1][find->x - 1] = '1';
-				free(find->next);
-				free(find);
-			}
-			else
-			{
-				tmp = character->P_coords;
-				character->P_coords = character->P_coords->next;
-				free(tmp);
-			}
-		}
-		if (i.i == character->C)
+		if(ft_check_exit(c_count, newmap, character))
+			return (0);
+		c_count = ft_check_c(map, newmap, character, c_count);
+		if (c_count == character->C)
 		{
 			ft_new_map(map, newmap);
-			i.i++;
+			c_count++;
 		}
 	}
 	return (ft_printf("Error\n\
