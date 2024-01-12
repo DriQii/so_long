4cab1029408623e8f5cb08e76b19980e2460aae4
Enzo Vella <DriQii>
2024-01-12 13:29:32 +0100

    f

---
+++ test/ft_strjoin.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evella <evella@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 23:06:41 by evella            #+#    #+#             */
+/*   Updated: 2023/10/01 23:13:01 by evella           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+static char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		len;
+
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1;
+	str = (char *)malloc(sizeof(char) * len);
+	if (!str)
+	{
+		return (NULL);
+	}
+	ft_strcpy(str, (char *)s1);
+	ft_strcpy(str + ft_strlen((char *) s1), (char *)s2);
+	return (str);
+}
